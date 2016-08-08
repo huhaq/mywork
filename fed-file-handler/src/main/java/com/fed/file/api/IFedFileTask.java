c0ddc6b19626a1346a99eb150051c632fb381a95
HAQH <HAQH@NW994758>
2016-08-07 20:14:59 -0400

    Added a comment

--- fed-file-handler/src/main/java/com/fed/file/api/IFedFileTask.java
+++ fed-file-handler/src/main/java/com/fed/file/api/IFedFileTask.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
 
+/**
+ * Comment
+ */
 public interface IFedFileTask {
     List<String> splitFileIntoWords(File file);
     
